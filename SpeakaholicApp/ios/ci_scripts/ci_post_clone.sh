6de672834bf488011feff4cdf7cd773c0bcbb5a5
Lester Privott <lprivott@compelone.com>
2023-01-24 22:15:10 -0500

    trying still

--- SpeakaholicApp/ios/ci_scripts/ci_post_clone.sh
+++ SpeakaholicApp/ios/ci_scripts/ci_post_clone.sh
@@ -11,11 +11,17 @@ brew install yarn
 
 # Install dependencies you manage with CocoaPods.
 
-ls -la
-
 yarn
 # npm install
 
+echo
+ls -la
+echo
+cd ios # for some reason I need to go into this directory now
+
+echo
+ls -la
+echo
 pod install
 # the sed command from RN cant find the file... so we have to run it ourselves
 # sed -i -e  $'s/ && (__IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_10_0)//' /Volumes/workspace/repository/ios/Pods/RCT-Folly/folly/portability/Time.h
