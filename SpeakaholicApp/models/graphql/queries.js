ce86587273d15071d6563fb4e705f4388e182a37
Lester Privott <lprivott@compelone.com>
2023-02-12 13:46:17 -0500

    Added graphql

---
+++ SpeakaholicApp/models/graphql/queries.js
@@ -0,0 +1,247 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const getUserCreditsLeft = /* GraphQL */ `
+  query GetUserCreditsLeft($id: ID!) {
+    getUserCreditsLeft(id: $id) {
+      id
+      created_date_utc
+      cognito_user_name
+      credits_left
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const listUserCreditsLefts = /* GraphQL */ `
+  query ListUserCreditsLefts(
+    $filter: ModelUserCreditsLeftFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUserCreditsLefts(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        created_date_utc
+        cognito_user_name
+        credits_left
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const syncUserCreditsLefts = /* GraphQL */ `
+  query SyncUserCreditsLefts(
+    $filter: ModelUserCreditsLeftFilterInput
+    $limit: Int
+    $nextToken: String
+    $lastSync: AWSTimestamp
+  ) {
+    syncUserCreditsLefts(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      lastSync: $lastSync
+    ) {
+      items {
+        id
+        created_date_utc
+        cognito_user_name
+        credits_left
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const getSpeechItems = /* GraphQL */ `
+  query GetSpeechItems($id: ID!) {
+    getSpeechItems(id: $id) {
+      id
+      s3_input_key
+      cognito_user_name
+      character_count
+      created_date_utc
+      is_processed
+      voice
+      language
+      prediction_type
+      s3_output_key
+      failed_reason
+      name
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const listSpeechItems = /* GraphQL */ `
+  query ListSpeechItems(
+    $filter: ModelSpeechItemsFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listSpeechItems(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        s3_input_key
+        cognito_user_name
+        character_count
+        created_date_utc
+        is_processed
+        voice
+        language
+        prediction_type
+        s3_output_key
+        failed_reason
+        name
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const syncSpeechItems = /* GraphQL */ `
+  query SyncSpeechItems(
+    $filter: ModelSpeechItemsFilterInput
+    $limit: Int
+    $nextToken: String
+    $lastSync: AWSTimestamp
+  ) {
+    syncSpeechItems(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      lastSync: $lastSync
+    ) {
+      items {
+        id
+        s3_input_key
+        cognito_user_name
+        character_count
+        created_date_utc
+        is_processed
+        voice
+        language
+        prediction_type
+        s3_output_key
+        failed_reason
+        name
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const getUsers = /* GraphQL */ `
+  query GetUsers($id: ID!) {
+    getUsers(id: $id) {
+      id
+      email
+      name
+      cognito_user_name
+      image_url
+      created_date_utc
+      monthly_limit
+      additional_credits
+      credit_reset_days
+      createdAt
+      updatedAt
+      _version
+      _deleted
+      _lastChangedAt
+    }
+  }
+`;
+export const listUsers = /* GraphQL */ `
+  query ListUsers(
+    $filter: ModelUsersFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listUsers(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        id
+        email
+        name
+        cognito_user_name
+        image_url
+        created_date_utc
+        monthly_limit
+        additional_credits
+        credit_reset_days
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
+export const syncUsers = /* GraphQL */ `
+  query SyncUsers(
+    $filter: ModelUsersFilterInput
+    $limit: Int
+    $nextToken: String
+    $lastSync: AWSTimestamp
+  ) {
+    syncUsers(
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+      lastSync: $lastSync
+    ) {
+      items {
+        id
+        email
+        name
+        cognito_user_name
+        image_url
+        created_date_utc
+        monthly_limit
+        additional_credits
+        credit_reset_days
+        createdAt
+        updatedAt
+        _version
+        _deleted
+        _lastChangedAt
+      }
+      nextToken
+      startedAt
+    }
+  }
+`;
