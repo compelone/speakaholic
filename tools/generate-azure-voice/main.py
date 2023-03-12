8dd987093d4026fb02a2febe6dbea719ef95df60
Lester Privott <lprivott@compelone.com>
2023-03-12 18:02:29 -0400

    Added additional speeches for youtube video

--- tools/generate-azure-voice/main.py
+++ tools/generate-azure-voice/main.py
@@ -13,7 +13,7 @@ voice = input()
 voice_name = 'en-US-%sNeural' % voice
 print('Using voice %s' % voice_name)
 audio_config = speechsdk.audio.AudioOutputConfig(
-    filename='/Users/lesterprivott/Downloads/ACCDownload_20230217152745/SSML/%s.mp3' % voice_name)
+    filename='/Users/lesterprivott/sc/speakaholic/tools/generate-azure-voice/%s.mp3' % voice_name)
 
 # The language of the voice that speaks.
 speech_config.speech_synthesis_voice_name = voice_name
@@ -22,17 +22,15 @@ speech_synthesizer = speechsdk.SpeechSynthesizer(
     speech_config=speech_config, audio_config=audio_config)
 
 # Get text from the console and synthesize to the default speaker.
-print("Enter some text that you want to speak >")
-text = 'Hi my name is %s, thank you for trying Speakaholic.' % voice
+# print("Enter some text that you want to speak >")
+# text = 'Hi my name is %s, thank you for trying Speakaholic.' % voice
 
-print('Speaking text > %s' % text)
-speech_synthesis_result = speech_synthesizer.speak_ssml_async("""<!--ID=B7267351-473F-409D-9765-754A8EBCDE05;Version=1|{"VoiceNameToIdMapItems":[{"Id":"93cdc1ca-0ba2-4615-91ec-495bd6b40b3d","Name":"Microsoft Server Speech Text to Speech Voice (en-US, JasonNeural)","ShortName":"en-US-JasonNeural","Locale":"en-US","VoiceType":"StandardVoice"}]}-->
+# print('Speaking text > %s' % text)
+text = '''<!--ID=B7267351-473F-409D-9765-754A8EBCDE05;Version=1|{"VoiceNameToIdMapItems":[{"Id":"93cdc1ca-0ba2-4615-91ec-495bd6b40b3d","Name":"Microsoft Server Speech Text to Speech Voice (en-US, JasonNeural)","ShortName":"en-US-JasonNeural","Locale":"en-US","VoiceType":"StandardVoice"}]}-->
 <!--ID=FCB40C2B-1F9F-4C26-B1A1-CF8E67BE07D1;Version=1|{"Files":{}}-->
 <!--ID=5B95B1CC-2C7B-494F-B746-CF22A0E779B7;Version=1|{"Locales":{"en-US":{"AutoApplyCustomLexiconFiles":[{}]}}}-->
-<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US"><voice name="en-US-JasonNeural"><s /><mstts:express-as style="Default"><prosody pitch="-10.00%">Introducing Speakaholic, the revolutionary new app that lets you communicate in a whole new way! With our cutting-edge text-to-speech and image-to-speech AI technology, you can transform any text or image into spoken content, with over 20 custom voices to choose from.</prosody></mstts:express-as><s />
-<s /><mstts:express-as style="Default"><prosody pitch="-10.00%">And now, for a limited time only, we're offering an exclusive promotion for new users. Download Speakaholic today and receive 1000 free characters with any character package purchase. That's right – 1000 characters free, and you can start using the app that is changing the way people communicate.</prosody></mstts:express-as><s />
-<s /><mstts:express-as style="Default"><prosody pitch="-10.00%">Whether you're a student, professional, or just someone who loves to stay connected, Speakaholic is the perfect tool for anyone who wants to make their voice heard. Our user-friendly interface makes it easy to input text or images and instantly generate spoken content, with a variety of voices to choose from.</prosody></mstts:express-as><s />
-<s /><mstts:express-as style="Default"><prosody pitch="-10.00%">Don't miss out on this incredible opportunity to try out Speakaholic at a fraction of the regular price. Download now and experience the power of our AI technology for yourself. With Speakaholic, you can communicate in a whole new way – and save money while you do it!</prosody></mstts:express-as><s /></voice></speak>""").get()
+<speak xmlns="http://www.w3.org/2001/10/synthesis" xmlns:mstts="http://www.w3.org/2001/mstts" xmlns:emo="http://www.w3.org/2009/10/emotionml" version="1.0" xml:lang="en-US"><voice name="en-US-JasonNeural"><s /><mstts:express-as style="Default"><prosody pitch="-10.00%">Welcome, to Byitll, LLC, we are pleased to bring you our new text to speech application, Speakaholic. With Speakaholic, we make it easy for you to enter text and convert it to speech, now you can make videos using custom voices, like this one. And with over 20 voices available we are sure you will find a voice that works for you and your text to speech projects. Stay tuned for more videos showing what Speakaholic can do!</prosody></mstts:express-as><s /></voice></speak>'''
+speech_synthesis_result = speech_synthesizer.speak_ssml_async(text).get()
 
 if speech_synthesis_result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
     print("Speech synthesized for text [{}]".format(text))
